702c6cb0e96ef07a96e774d7bd1873dd17c5b786
MuhammadNoorS <80032299+Saputro-bit@users.noreply.github.com>
2022-05-19 18:55:06 +0700

    first commit

---
+++ src/main/java/uno/jogja/project/ktp/DataJpaController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uno.jogja.project.ktp;
+
+/**
+ *
+ * @author asus
+ */
+public class DataJpaController {
+
+    int getDataCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object findDataEntities() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
